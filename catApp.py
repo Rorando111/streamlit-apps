62fd1cf6bb93f8db827c75d095fbc1bd1acd4a6f
Rorando111 <158253292+Rorando111@users.noreply.github.com>
2024-05-18 00:24:39 +0800

    catApp.py

--- catApp.py
+++ catApp.py
@@ -1,42 +1,36 @@
 import streamlit as st
 import tensorflow as tf
-import cv2
-from PIL import Image, ImageOps
-import numpy as np
-
-class_names = ['Abyssinian', 'Bengal', 'Birman', 'Bombay',
-               'British Shorthair', 'Egyptian Mau', 'Maine Coon',
-               'Norweigian Forest', 'Persian', 'Ragdoll',
-               'Russian Blue', 'Siamese', 'Sphynx']
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-    model = tf.keras.models.load_model('cat_breed_classifier.h5')
-    return model
+  model=tf.keras.models.load_model('cat_breed_classifier.h5')
+  return model
+model=load_model()
+st.write("""
+# Cat Breed Classifier"""
+)
+file=st.file_uploader("Choose plant photo from computer",type=["jpg","jpeg","png"])
 
-with st.spinner('Model is being loaded..'):
-    model = load_model()
-
-st.write("# Cat Breed Classification")
-
-file = st.file_uploader("Please upload a cat image", type=["jpg", "jpeg", "png"])
-
-def import_and_predict(image_data, model):
-    size = (224, 224)    
-    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
-    image = np.asarray(image)
-    img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    img_reshape = img[np.newaxis,...]
-    prediction = model.predict(img_reshape)
+import cv2
+from PIL import Image,ImageOps
+import numpy as np
+def import_and_predict(image_data,model):
+    size=(64,64)
+    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
+    img=np.asarray(image)
+    img_reshape=img[np.newaxis,...]
+    prediction=model.predict(img_reshape)
     return prediction
-
 if file is None:
     st.text("Please upload an image file")
 else:
-    image = Image.open(file)
-    st.image(image, use_column_width=True)
-    predictions = import_and_predict(image, model)
-    score = tf.nn.softmax(predictions[0])
-    st.write(predictions)
-    st.write(score)
-    st.write("This image most likely belongs to the {} breed with a {:.2f}% confidence.".format(class_names[np.argmax(score)], 100 * np.max(score)))
+    image=Image.open(file)
+    st.image(image,use_column_width=True)
+    prediction=import_and_predict(image,model)
+    
+class_names = ['Abyssinian', 'Bengal', 'Birman', 'Bombay',
+               'British Shorthair', 'Egyptian Mau', 'Maine Coon',
+               'Norweigian forest', 'Persian', 'Ragdoll',
+               'Russian Blue', 'Siamese', 'Sphynx']
+    string="OUTPUT : "+class_names[np.argmax(prediction)]
+    st.success(string)
