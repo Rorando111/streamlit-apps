dad3b09d48a311b98ce905460fa7abc57e33632a
Rorando111 <158253292+Rorando111@users.noreply.github.com>
2024-05-17 08:23:33 +0800

    catApp.py

--- catApp.py
+++ catApp.py
@@ -1,11 +1,5 @@
 import streamlit as st
 import tensorflow as tf
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.losses import CategoricalCrossentropy
-from tensorflow.keras.metrics import CategoricalAccuracy
 import os
 import numpy as np
 
