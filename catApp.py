0faf9872ce9caf5f724202cfd05da01ee3cd39be
Rorando111 <158253292+Rorando111@users.noreply.github.com>
2024-05-18 00:49:39 +0800

    catApp.py

--- catApp.py
+++ catApp.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import tensorflow as tf
+from PIL import Image, ImageOps
+import cv2
+import numpy as np
 
 class_names = ['Abyssinian', 'Bengal', 'Birman', 'Bombay',
                'British Shorthair', 'Egyptian Mau', 'Maine Coon',
@@ -8,10 +11,11 @@ class_names = ['Abyssinian', 'Bengal', 'Birman', 'Bombay',
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model=tf.keras.models.load_model('cat_breed_classifier.h5')
-  return model
+    model=tf.keras.models.load_model('cat_breed_classifier.h5')
+    return model
+
 with st.spinner('Model is being loaded..'):
-  model=load_model()
+    model=load_model()
 
 st.write("""
          # Cat Breed Classification
@@ -19,34 +23,26 @@ st.write("""
          )
 
 file = st.file_uploader("Please upload cat image from device", type=["jpg", "jpeg", "png"])
-import cv2
-from PIL import Image, ImageOps
-import numpy as np
-st.set_option('deprecation.showfileUploaderEncoding', False)
+
 def import_and_predict(image_data, model):
-    
-        size = (224,224)    
-        image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
-        image = np.asarray(image)
-        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        #img_resize = (cv2.resize(img, dsize=(75, 75), interpolation=cv2.INTER_CUBIC))/255.
-        
-        img_reshape = img[np.newaxis,...]
-    
-        prediction = model.predict(img_reshape)
-        
-        return prediction
+    size = (224,224)    
+    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+    image = np.asarray(image)
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    img_reshape = img[np.newaxis,...]
+    predictions = model.predict(img_reshape)
+    score = tf.nn.softmax(predictions[0])
+    return predictions, score
+
 if file is None:
     st.text("Please upload an image file")
 else:
     image = Image.open(file)
     st.image(image, use_column_width=True)
-    predictions = import_and_predict(image, model)
-    score = tf.nn.softmax(predictions[0])
-    st.write(predictions)
-    st.write(score)
-    print(
-    "This image most likely belongs to {} with a {:.2f} percent confidence."
-    .format(class_names[np.argmax(score)], 100 * np.max(score))
-)
-
+    predictions, score = import_and_predict(image, model)
+    st.write("Predictions:", predictions)
+    st.write("Scores:", score)
+    st.success(
+        "This image most likely belongs to {} with a {:.2f} percent confidence."
+        .format(class_names[np.argmax(score)], 100 * np.max(score))
+    )
