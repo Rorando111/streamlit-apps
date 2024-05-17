67b993f0c385dbaf6e2d13b17940f0bf62626bf2
Rorando111 <158253292+Rorando111@users.noreply.github.com>
2024-05-18 01:21:46 +0800

    catApp.py

--- catApp.py
+++ catApp.py
@@ -3,10 +3,15 @@ import tensorflow as tf
 from PIL import Image, ImageOps
 import numpy as np
 
+# Get the predicted breed and probability
+class_names = ['Abyssinian', 'Bengal', 'Birman', 'Bombay',
+               'British Shorthair', 'Egyptian Mau', 'Maine Coon',
+               'Norweigian forest', 'Persian', 'Ragdoll',
+               'Russian Blue', 'Siamese', 'Sphynx']
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model=tf.keras.models.load_model('cat_breed_classifier.h5')
-  return model
+    model=tf.keras.models.load_model('cat_breed_classifier.h5')
+    return model
 
 model=load_model()
 
@@ -33,16 +38,10 @@ else:
     # Make the prediction
     prediction = model.predict(img_reshape)
 
-    # Get the predicted breed and probability
-    class_names = ['Abyssinian', 'Bengal', 'Birman', 'Bombay',
-                   'British Shorthair', 'Egyptian Mau', 'Maine Coon',
-                   'Norweigian forest', 'Persian', 'Ragdoll',
-                   'Russian Blue', 'Siamese', 'Sphynx']
-    predicted_breed = class_names[np.argmax(prediction)]
-    probability = np.max(prediction)
+    
+    predicted_breed = class_names[np.argmax(prediction[0])]
+    probability = np.max(prediction[0])
 
     # Display the results
     string="OUTPUT : {} ({:.2f}%)".format(predicted_breed, probability*100)
     st.success(string)
-
-    
