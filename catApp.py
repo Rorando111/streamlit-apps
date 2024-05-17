e0e92fd64664fd0400e1efc97995db4beeae3274
Rorando111 <158253292+Rorando111@users.noreply.github.com>
2024-05-18 01:18:14 +0800

    catApp.py

--- catApp.py
+++ catApp.py
@@ -1,48 +1,48 @@
 import streamlit as st
 import tensorflow as tf
 from PIL import Image, ImageOps
-import cv2
 import numpy as np
-
-class_names = ['Abyssinian', 'Bengal', 'Birman', 'Bombay',
-               'British Shorthair', 'Egyptian Mau', 'Maine Coon',
-               'Norweigian forest', 'Persian', 'Ragdoll',
-               'Russian Blue', 'Siamese', 'Sphynx']
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-    model=tf.keras.models.load_model('cat_breed_classifier.h5')
-    return model
+  model=tf.keras.models.load_model('cat_breed_classifier.h5')
+  return model
 
-with st.spinner('Model is being loaded..'):
-    model=load_model()
+model=load_model()
 
 st.write("""
-         # Cat Breed Classification
-         """
-         )
+# Cat Breed Classifier
 
-file = st.file_uploader("Please upload cat image from device", type=["jpg", "jpeg", "png"])
+Upload an image of a cat to predict its breed.
+""")
 
-def import_and_predict(image_data, model):
-    size = (224,224)    
-    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
-    image = np.asarray(image)
-    img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    img_reshape = img[np.newaxis,...]
-    predictions = model.predict(img_reshape)
-    score = tf.nn.softmax(predictions[0])
-    return predictions, score
+file=st.file_uploader("Choose cat photo from device",type=["jpg","jpeg","png"])
 
 if file is None:
     st.text("Please upload an image file")
 else:
-    image = Image.open(file)
-    st.image(image, use_column_width=True)
-    predictions, score = import_and_predict(image, model)
-    st.write("Predictions:", predictions)
-    st.write("Scores:", score)
-    st.success(
-        "This image most likely belongs to {} with a {:.2f} percent confidence."
-        .format(class_names[np.argmax(score)], 100 * np.max(score))
-    )
+    image=Image.open(file)
+    st.image(image,use_column_width=True)
+
+    # Preprocess the image
+    size=(224,224)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+    img = np.asarray(image) / 255.0
+    img_reshape = np.expand_dims(img, axis=0)
+
+    # Make the prediction
+    prediction = model.predict(img_reshape)
+
+    # Get the predicted breed and probability
+    class_names = ['Abyssinian', 'Bengal', 'Birman', 'Bombay',
+                   'British Shorthair', 'Egyptian Mau', 'Maine Coon',
+                   'Norweigian forest', 'Persian', 'Ragdoll',
+                   'Russian Blue', 'Siamese', 'Sphynx']
+    predicted_breed = class_names[np.argmax(prediction)]
+    probability = np.max(prediction)
+
+    # Display the results
+    string="OUTPUT : {} ({:.2f}%)".format(predicted_breed, probability*100)
+    st.success(string)
+
+    
