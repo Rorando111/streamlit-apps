495a3bbd4903df5a123039aa869faaaaa4737222
Rorando111 <158253292+Rorando111@users.noreply.github.com>
2024-05-18 12:44:08 +0800

    catApp.py

--- catApp.py
+++ catApp.py
@@ -1,36 +1,44 @@
 import streamlit as st
 import tensorflow as tf
+import cv2
+from PIL import Image, ImageOps
+import numpy as np
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model = tf.keras.models.load_model('cat_classifier.h5')
-  return model
-model=load_model()
-st.write("""
-# Cat Breed Classifier"""
-)
-file=st.file_uploader("Upload an image of a cat to classify its breed:",type=["jpg","png"])
+    custom_objects = {'BatchNormalization': tf.keras.layers.BatchNormalization}
+    model = tf.keras.models.load_model('cat_classifier.h5', custom_objects=custom_objects)
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
 
-import cv2
-from PIL import Image,ImageOps
-import numpy as np
-def import_and_predict(image_data,model):
-    size=(224,224)
-    image=ImageOps.fit(image_data,size,Image.LANCZOS)
-    img=np.asarray(image)
-    img_reshape=img[np.newaxis,...]
-    prediction=model.predict(img_reshape)
-    return prediction
+model = load_model()
+
+st.write("""
+# Cat Breed Classifier
+""")
+
+file = st.file_uploader("Upload an image of a cat to classify its breed:", type=["jpg", "png"])
+
 if file is None:
     st.text("Please upload an image file")
 else:
-    image=Image.open(file)
-    st.image(image,use_column_width=True)
-    prediction=import_and_predict(image,model)
-    class_names = ['Abyssinian', 'Bengal', 'Birman', 'Bombay',
-               'British Shorthair', 'Egyptian Mau', 'Maine Coon',
-               'Norweigian forest', 'Persian', 'Ragdoll',
-               'Russian Blue', 'Siamese', 'Sphynx']
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    size = (224, 224)
 
-    string="OUTPUT : "+class_names[np.argmax(prediction)]
+    def import_and_predict(image_data, model):
+        image = ImageOps.fit(image_data, size, Image.LANCZOS)
+        img = np.asarray(image)
+        img = img / 255.0
+        img_reshape = img[np.newaxis, ...]
+        prediction = model.predict(img_reshape)
+        return prediction
+
+    prediction = import_and_predict(image, model)
+    class_names = ['Abyssinian', 'Bengal', 'Birman', 'Bombay',
+                     'British Shorthair', 'Egyptian Mau', 'Maine Coon',
+                     'Norweigian forest', 'Persian', 'Ragdoll',
+                     'Russian Blue', 'Siamese', 'Sphynx']
+
+    string = "OUTPUT : " + class_names[np.argmax(prediction)]
     st.success(string)
