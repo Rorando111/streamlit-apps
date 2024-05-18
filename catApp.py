b290bd7197acb50066e0729326e220356bc930de
Rorando111 <158253292+Rorando111@users.noreply.github.com>
2024-05-18 12:18:20 +0800

    catApp.py

--- catApp.py
+++ catApp.py
@@ -1,35 +1,21 @@
 import streamlit as st
 import tensorflow as tf
-from tensorflow.keras.layers import BatchNormalization
-
-# Custom object configuration for deserialization
-custom_objects = {'BatchNormalization': BatchNormalization}
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-    model = tf.keras.models.load_model('cat_classifier.hdf5', custom_objects=custom_objects)
-    return model
+  model = tf.keras.models.load_model('cat_classifier.h5')
+  return model
 
 model = load_model()
 st.write("""
-# Cat Breed Classifier
-""")
+# Cat Breed Classifier"""
+)
 
 file = st.file_uploader("Upload an image of a cat to classify its breed:", type=["jpg", "jpeg", "png"])
 
 import cv2
-from PIL import Image, ImageOps
+from PIL import Image,ImageOps
 import numpy as np
-
-def import_and_predict(image_data, model):
-    size = (224, 224)
-    image = ImageOps.fit(image_data, size, Image.LANCZOS)
-    img = np.asarray(image)
-    img = img / 255.0  # Normalize pixel values
-    img_reshape = np.expand_dims(img, axis=0)
-    prediction = model.predict(img_reshape)
-    return prediction
-
 if file is not None:
     image = Image.open(file)
     st.image(image, use_column_width=True)
@@ -43,3 +29,15 @@ if file is not None:
     # Display the result
     st.write(f"Class label: {class_label}")
     st.write(f"Confidence: {img_reshape[0, class_label]:.2f}")
+
+def import_and_predict(image_data, model):
+    size = (224, 224)
+    image = ImageOps.fit(image_data, size, Image.LANCZOS)
+    img = np.asarray(image)
+    img = img / 255.0  # Normalize pixel values
+    img_reshape = np.expand_dims(img, axis=0)
+    prediction = model.predict(img_reshape)
+    return prediction
+
+# Save the model using the `save_model` function
+tf.keras.models.save_model(model, 'cat_classifier.h5')
