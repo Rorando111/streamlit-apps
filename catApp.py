d8b6d6fcda365caea174bc7ec5c7c4f4877d0453
Rorando111 <158253292+Rorando111@users.noreply.github.com>
2024-05-18 15:42:06 +0800

    catApp.py

--- catApp.py
+++ catApp.py
@@ -1,44 +1,42 @@
+
 import streamlit as st
-import tensorflow as tf
-import cv2
-from PIL import Image, ImageOps
+from PIL import Image
+from keras.preprocessing.image import load_img,img_to_array
 import numpy as np
+from keras.models import load_model
 
-@st.cache(allow_output_mutation=True)
-def load_model():
-    custom_objects = {'BatchNormalization': tf.keras.layers.BatchNormalization}
-    model = tf.keras.models.load_model('cat_classifier.h5', custom_objects=custom_objects)
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    return model
+model = load_model('cat_classifier.h5',compile=False)
+lab = {'Abyssinian': 0, 'Bengal': 1, 'Birman': 2, 'Bombay': 3, 'British Shorthair': 4, 'Egyptian Mau': 5, 'Maine Coon': 6, 'Norwegian Forest': 7, 'Persian': 8, 'Ragdoll': 9, 'Russian Blue': 10, 'Siamese': 11, 'Sphynx': 12}
 
-model = load_model()
+def processed_img(img_path):
+    img=load_img(img_path,target_size=(224,224,3))
+    img=img_to_array(img)
+    img=img/255
+    img=np.expand_dims(img,[0])
+    answer=model.predict(img)
+    y_class = answer.argmax(axis=-1)
+    print(y_class)
+    y = " ".join(str(x) for x in y_class)
+    y = int(y)
+    res = lab[y]
+    print(res)
+    return res
 
-st.write("""
-# Cat Breed Classifier
-""")
+def run():
+    img1 = img1.resize((224,224))
+    st.image(img1,use_column_width=False)
+    st.title("Cat Breed Classification")
+    st.markdown('''<h4 style='text-align: left; color: #d73b5c;'>* Data is based "270 Bird Species also see 70 Sports Dataset"</h4>''',
+                unsafe_allow_html=True)
 
-file = st.file_uploader("Upload an image of a cat to classify its breed:", type=["jpg", "png"])
+    img_file = st.file_uploader("Choose an Image of Bird", type=["jpg", "png"])
+    if img_file is not None:
+        st.image(img_file,use_column_width=False)
+        save_image_path = './upload_images/'+img_file.name
+        with open(save_image_path, "wb") as f:
+            f.write(img_file.getbuffer())
 
-if file is None:
-    st.text("Please upload an image file")
-else:
-    image = Image.open(file)
-    st.image(image, use_column_width=True)
-    size = (224, 224)
-
-    def import_and_predict(image_data, model):
-        image = ImageOps.fit(image_data, size, Image.LANCZOS)
-        img = np.asarray(image)
-        img = img / 255.0
-        img_reshape = img[np.newaxis, ...]
-        prediction = model.predict(img_reshape)
-        return prediction
-
-    prediction = import_and_predict(image, model)
-    class_names = ['Abyssinian', 'Bengal', 'Birman', 'Bombay',
-                     'British Shorthair', 'Egyptian Mau', 'Maine Coon',
-                     'Norweigian forest', 'Persian', 'Ragdoll',
-                     'Russian Blue', 'Siamese', 'Sphynx']
-
-    string = "OUTPUT : " + class_names[np.argmax(prediction)]
-    st.success(string)
+        if st.button("Predict"):
+            result = processed_img(save_image_path)
+            st.success("Predicted Cat breed is: "+result)
+run()
