1302e36337d9111b245a221b0bb4a6f2f2fe7b91
Rorando111 <158253292+Rorando111@users.noreply.github.com>
2024-05-18 12:33:22 +0800

    catApp.py

--- catApp.py
+++ catApp.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model = tf.keras.models.load_model('cat_classifier.h5')
+  model = tf.keras.models.load_model('model.hdf5')
   return model
 
 model = load_model()
